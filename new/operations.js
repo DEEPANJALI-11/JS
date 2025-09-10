2b2df1c36be34e5736c995c61739a667a32b421f
Deepanjali Singh <113447060+DEEPANJALI-11@users.noreply.github.com>
2025-09-10 06:03:41 +0000

    operations

---
+++ new/operations.js
@@ -0,0 +1,27 @@
+let value=3
+let negvalue=-value
+console.log(negvalue)
+console.log(3**3)
+let str1="Deeepanjali"
+let str2=" Singh"
+str3=str1+str2
+console.log(str3)
+
+console.log(1+"2"); //12
+console.log("1"+2)  //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32 
+
+console.log(true) //true
+console.log(+true) //1
+console.log(+"") //0 
+
+let x=100
+++x
+console.log(x); //101
+x++
+console.log(x); //102
+
+
+
+
