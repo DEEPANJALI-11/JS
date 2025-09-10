3c1cff36dcb2b9a16878a51730def18411bddc2d
Deepanjali Singh <113447060+DEEPANJALI-11@users.noreply.github.com>
2025-09-10 08:30:09 +0000

    changes

--- new/07_date.js
+++ new/07_date.js
@@ -1,4 +1,4 @@
-/*let myDate=new Date()
+let myDate=new Date()
 console.log(myDate); //2025-09-10T08:13:44.226Z
 console.log(myDate.toString()); //Wed Sep 10 2025 08:14:18 GMT+0000 (Coordinated Universal Time)
 console.log(myDate.toDateString()); //Wed Sep 10 2025
@@ -12,11 +12,11 @@ let newdate=new Date(2023,0,9,5,3)
 console.log(newdate.toLocaleString()); //1/9/2023, 5:03:00 AM
 let newdate2=new Date("2023-01-14")
 console.log(newdate2); //2023-01-14T00:00:00.000Z
-*/
-// let d=new Date("01-14-2023")
-// let myTimeStamp=Date.now() 
-// console.log(myTimeStamp);  //1757492596621
-// console.log(d.getTime());  //1673654400000
+
+let d=new Date("01-14-2023")
+let myTimeStamp=Date.now() 
+console.log(myTimeStamp);  //1757492596621
+console.log(d.getTime());  //1673654400000
 
 console.log(Math.floor(Date.now()/1000)); //to get op in seconds
 
